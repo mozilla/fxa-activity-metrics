e40bbf771aafc95d0b2012cf6f7137651a8b3768
Phil Booth <pmbooth@gmail.com>
2016-09-10 14:47:46 +0100

    Revert "wip: try ditching default values to fix ROW expression problem"
    This reverts commit b6f504c4fece85383c435200889a4ea8f111df7c.

--- import_flow_events.py
+++ import_flow_events.py
@@ -62,12 +62,12 @@ Q_CREATE_CSV_TABLE = """
 Q_CREATE_METADATA_TABLE = """
     CREATE TABLE IF NOT EXISTS flow_metadata (
       flowId VARCHAR(64) NOT NULL UNIQUE,
-      beginTime TIMESTAMP NOT NULL,
+      beginTime TIMESTAMP NOT NULL DEFAULT '1970-01-01'::TIMESTAMP SORTKEY,
       -- Ideally duration would be type INTERVAL
       -- but redshift doesn't support that.
-      duration INTEGER NOT NULL,
-      completed BOOLEAN NOT NULL,
-      newAccount BOOLEAN NOT NULL,
+      duration INTEGER NOT NULL DEFAULT 0,
+      completed BOOLEAN NOT NULL DEFAULT FALSE,
+      newAccount BOOLEAN NOT NULL DEFAULT FALSE,
       uaBrowser VARCHAR(40),
       uaVersion VARCHAR(40),
       uaOS VARCHAR(40),
@@ -84,7 +84,7 @@ Q_CREATE_METADATA_TABLE = """
 """
 Q_CREATE_EVENTS_TABLE = """
     CREATE TABLE IF NOT EXISTS flow_events (
-      timestamp TIMESTAMP NOT NULL,
+      timestamp TIMESTAMP NOT NULL DEFAULT '1970-01-01'::TIMESTAMP SORTKEY,
       -- Ideally flowTime would be type INTERVAL
       -- but redshift doesn't support that.
       flowTime INTEGER NOT NULL,
@@ -138,10 +138,6 @@ Q_COPY_CSV = """
 Q_INSERT_METADATA = """
     INSERT INTO flow_metadata (
       flowId,
-      beginTime,
-      duration,
-      completed,
-      newAccount,
       uaBrowser,
       uaVersion,
       uaOS,
@@ -157,10 +153,6 @@ Q_INSERT_METADATA = """
     )
     SELECT (
       flowId,
-      '1970-01-01'::TIMESTAMP,
-      0,
-      FALSE,
-      FALSE,
       uaBrowser,
       uaVersion,
       uaOS,
@@ -221,13 +213,11 @@ Q_UPDATE_NEW_ACCOUNT = """
 
 Q_INSERT_EVENTS = """
     INSERT INTO flow_events (
-      timestamp,
       flowTime,
       flowId,
       type
     )
     SELECT (
-      '1970-01-01'::TIMESTAMP,
       flowTime,
       flowId,
       type

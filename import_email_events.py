ba459656f1ec02c59db59fe9fb086855b2a4a50a
Vijay Budhram <vbudhram@mozilla.com>
2017-04-12 11:30:10 -0400

    feat(scripts): Fixes from @philbooth review

--- import_email_events.py
+++ import_email_events.py
@@ -4,15 +4,14 @@
 
 import import_events
 SCHEMA = """
-    timestamp BIGINT NOT NULL SORTKEY,
     type VARCHAR(30) NOT NULL,
     locale VARCHAR(40),
     domain VARCHAR(40),
-    template VARCHAR(64)
+    template VARCHAR(64),
     flow_id VARCHAR(64)
 """
 
-COLUMNS = "timestamp, type, locale, domain, template, flow_id"
+COLUMNS = "type, locale, domain, template, flow_id"
 
 import_events.run("fxa-email", "email", SCHEMA, COLUMNS, SCHEMA, COLUMNS)
 

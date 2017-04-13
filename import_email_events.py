3ac5dfd8d3ddd2c83e5e0b1939ac7ebddc1d669b
Vijay Budhram <vbudhram@mozilla.com>
2017-04-12 21:42:50 -0400

    feat(scripts): Add import email script

---
+++ import_email_events.py
@@ -0,0 +1,24 @@
+#
+# Script to import "email event" metrics from S3 into redshift.
+#
+
+import import_events
+SCHEMA = """
+    flow_id VARCHAR(64) DISTKEY ENCODE lzo,
+    domain VARCHAR(40) ENCODE lzo,
+    template VARCHAR(64) ENCODE lzo,
+    type VARCHAR(64) NOT NULL ENCODE lzo,
+    complaint VARCHAR(64) ENCODE lzo,
+    bounced VARCHAR(64) ENCODE lzo,
+    locale VARCHAR(64) ENCODE lzo
+"""
+
+COLUMNS = "flow_id, domain, template, type, complaint, bounced, locale"
+
+import_events.run(s3_prefix="fxa-email/data/email-events",
+                  event_type="email",
+                  temp_schema=SCHEMA,
+                  temp_columns=COLUMNS,
+                  perm_schema=SCHEMA,
+                  perm_columns=COLUMNS,
+                  id_column="flow_id")
